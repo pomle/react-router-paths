7bd73e525b0f0564b52c7154b95101f00344382d
Pontus Alexander <pontus.alexander@gmail.com>
2022-09-15 01:05:34 +0200

    Only mount on exact match

--- src/components/__tests__/PathRoute.test.tsx
+++ src/components/__tests__/PathRoute.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { codecs, createPath } from '@pomle/paths';
 import renderer from 'react-test-renderer';
 import { createContext } from '../../mocks/context';
-import { PathRoute } from '../PathRoute';
+import { PathRoute, mount } from '../PathRoute';
 
 describe('PathRoute', () => {
   const path = createPath('/my/path/:with/:count', {
@@ -64,6 +64,65 @@ describe('PathRoute', () => {
     expect(spy).toHaveBeenCalledWith({
       exact: false,
       params: { count: 16, with: 'bar' },
+    });
+  });
+
+  describe('#mount', () => {
+    it('mounts a component and renders on exact match', () => {
+      const { Component } = createContext(['/my/path/bar/16']);
+      const spy = jest.fn();
+
+      function RouteRender(props: { count: number; with: string }) {
+        spy(props);
+        return <div />;
+      }
+
+      renderer.create(
+        <Component>
+          <PathRoute path={path}>{mount(RouteRender)}</PathRoute>
+        </Component>,
+      );
+
+      expect(spy).toHaveBeenCalledWith({
+        count: 16,
+        with: 'bar',
+      });
+    });
+
+    it('does not mount a component with overmatch', () => {
+      const { Component } = createContext(['/my/path/bar/16/with/over/match']);
+      const spy = jest.fn();
+
+      function RouteRender(props: { count: number; with: string }) {
+        spy(props);
+        return <div />;
+      }
+
+      renderer.create(
+        <Component>
+          <PathRoute path={path}>{mount(RouteRender)}</PathRoute>
+        </Component>,
+      );
+
+      expect(spy).toHaveBeenCalledTimes(0);
+    });
+
+    it('does not mount a component with undermatch', () => {
+      const { Component } = createContext(['/my/path']);
+      const spy = jest.fn();
+
+      function RouteRender(props: { count: number; with: string }) {
+        spy(props);
+        return <div />;
+      }
+
+      renderer.create(
+        <Component>
+          <PathRoute path={path}>{mount(RouteRender)}</PathRoute>
+        </Component>,
+      );
+
+      expect(spy).toHaveBeenCalledTimes(0);
     });
   });
 });
