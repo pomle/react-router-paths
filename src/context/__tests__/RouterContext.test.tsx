b3796cb7c606402fe845a00dbe0abc3b886c2141
Pontus Alexander <pontus.alexander@gmail.com>
2023-07-19 09:38:50 +0200

    Add test for go, back, forward

--- src/context/__tests__/RouterContext.test.tsx
+++ src/context/__tests__/RouterContext.test.tsx
@@ -3,7 +3,7 @@ import { createContext } from '../../mocks/context';
 import { createPath } from '@pomle/paths';
 import { useNav } from '../../hooks/useNav';
 import { create, act } from 'react-test-renderer';
-import { useLocation } from '../RouterContext';
+import { useHistory, useLocation } from '../RouterContext';
 
 describe('RouterContext', () => {
   const path = createPath('/my/path', {});
@@ -36,5 +36,70 @@ describe('RouterContext', () => {
     expect(history.length).toBe(2);
     expect(history.entries[1].pathname).toBe('/my/path');
     expect(tree.toJSON()).toEqual('/my/path');
+  });
+
+  it('supports go, back, and next calls', async () => {
+    const { Component, history } = createContext([
+      '/a/1',
+      '/b/2',
+      '/c/3',
+      '/d/4',
+      '/e/5',
+      '/f/6',
+    ]);
+
+    function Content() {
+      const { go, back, forward } = useHistory();
+
+      const { pathname } = useLocation();
+
+      return (
+        <>
+          <div data-what='loc'>{pathname}</div>
+          <button data-what='back' onClick={back}>
+            Back
+          </button>
+          <button data-what='forw' onClick={forward}>
+            Forward
+          </button>
+
+          <button data-what='jump' onClick={() => go(3)}>
+            Jump
+          </button>
+        </>
+      );
+    }
+
+    const tree = create(
+      <Component>
+        <Content />
+      </Component>,
+    );
+
+    expect(history.length).toBe(6);
+
+    act(() => {
+      tree.root.findByProps({ 'data-what': 'forw' }).props.onClick();
+    });
+
+    expect(tree.root.findByProps({ 'data-what': 'loc' }).children).toEqual([
+      '/b/2',
+    ]);
+
+    act(() => {
+      tree.root.findByProps({ 'data-what': 'back' }).props.onClick();
+    });
+
+    expect(tree.root.findByProps({ 'data-what': 'loc' }).children).toEqual([
+      '/a/1',
+    ]);
+
+    act(() => {
+      tree.root.findByProps({ 'data-what': 'jump' }).props.onClick();
+    });
+
+    expect(tree.root.findByProps({ 'data-what': 'loc' }).children).toEqual([
+      '/d/4',
+    ]);
   });
 });
