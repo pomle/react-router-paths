2abc57fb8eaff3c318d625499a8186d44a448183
Pontus Alexander <pontus.alexander@gmail.com>
2024-09-09 13:43:20 +0200

    Expand test suite

--- src/lib/__tests__/query.test.ts
+++ src/lib/__tests__/query.test.ts
@@ -32,14 +32,50 @@ describe.only('Stable Query Parser', () => {
     expect(firstParse.date[0]).not.toBe(secondParse.date[0]);
   });
 
-  it('forgets references based on args that changed', () => {
+  it('returns a new reference even when args change back to previous known values', () => {
     const parse = createParser(query);
-    const firstParse = parse('date=100000000&date=350000000');
+    const firstParse = parse('date=100000000');
     const secondParse = parse('date=200000000');
-    const thirdParse = parse('date=100000000&date=350000000');
+    const thirdParse = parse('date=100000000');
     expect(firstParse.date[0]).not.toBe(thirdParse.date[0]);
     expect(firstParse.date[0]).not.toBe(secondParse.date[0]);
     expect(secondParse.date[0]).not.toBe(thirdParse.date[0]);
+  });
+
+  it('forgets arguments that were removed', () => {
+    const parse = createParser(query);
+    const firstParse = parse('date=100000000&date=200000000');
+    parse('');
+    const thirdParse = parse('date=100000000&date=200000000');
+    expect(firstParse.date[0]).not.toBe(thirdParse.date[0]);
     expect(firstParse.date[1]).not.toBe(thirdParse.date[1]);
+  });
+
+  it('continually returns stable refs while arguments do not change', () => {
+    const parse = createParser(query);
+    const firstParse = parse('date=100000000&date=200000000');
+    const secondParse = parse('date=100000000&date=200000000');
+    const thirdParse = parse('date=100000000&date=200000000');
+    const fourthParse = parse('date=100000000&date=200000000');
+    expect(secondParse.date[0]).toBe(firstParse.date[0]);
+    expect(secondParse.date[1]).toBe(firstParse.date[1]);
+    expect(thirdParse.date[0]).toBe(firstParse.date[0]);
+    expect(thirdParse.date[1]).toBe(firstParse.date[1]);
+    expect(fourthParse.date[0]).toBe(firstParse.date[0]);
+    expect(fourthParse.date[1]).toBe(firstParse.date[1]);
+  });
+
+  it('returns stable arguments when other aspects change', () => {
+    const parse = createParser(query);
+    const firstParse = parse('date=100000000&date=200000000&date=1');
+    const secondParse = parse('date=100000000&foo=bar&date=200000000');
+    const thirdParse = parse('&foo=bar&foo=bar&date=100000000&date=200000000');
+    const fourthParse = parse('date=100000000&date=200000000');
+    expect(secondParse.date[0]).toBe(firstParse.date[0]);
+    expect(secondParse.date[1]).toBe(firstParse.date[1]);
+    expect(thirdParse.date[0]).toBe(firstParse.date[0]);
+    expect(thirdParse.date[1]).toBe(firstParse.date[1]);
+    expect(fourthParse.date[0]).toBe(firstParse.date[0]);
+    expect(fourthParse.date[1]).toBe(firstParse.date[1]);
   });
 });
